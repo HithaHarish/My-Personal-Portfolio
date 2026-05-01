30e268f2e2e05cd3be2c91dff6afd57f552f119d
Hitha Harish <hitha22harish@gmail.com>
2026-05-01 23:53:28 +0530

    Update experience title to 'My Development Journey'

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -27,7 +27,7 @@ export default function Experience() {
 
   return (
     <div className="experience-container">
-      <h2 className="experience-title">I'm experienced in..</h2>
+      <h2 className="experience-title">My Development Journey</h2>
 
       {experienceData.map((exp, index) => (
         <div
