d1f488bf8b73f1384795fd6efc36aae30d70904b
Hitha Harish <hitha22harish@gmail.com>
2026-05-02 00:30:03 +0530

    Update achievementsData.js

--- src/data/achievementsData.js
+++ src/data/achievementsData.js
@@ -3,7 +3,7 @@ export const achievementsData = {
   profileImage: "/assets/Achievements/profileimg.jpeg",
   descriptionPoints: [
     "I'm a 3rd-year Computer Science Engineering student at BMS College of Engineering, Bengaluru",
-    "I believe in Learning Continuously, Growing Consistently"
+    "I believe in Learning Continuously, Growing Consistently",
     "I build until it makes sense."
   ],
   achievements: [
